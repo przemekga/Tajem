392fbc015317526cd9f8a2cf55e5e6070f149699
pemol117 <pemol10@gmail.com>
2017-02-16 23:10:53 +0100

    ini

---
+++ js/carousel.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.slider-track').slick({
+        // prevArrow: $('.arr-left'),
+        // nextArrow: $('.arr-right'),
+        infinite: true,
+        speed: 500
+    });
+});
